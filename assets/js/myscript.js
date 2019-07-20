6b472ad3e92e73b6c2e9cd48eff6912867be230b
Bikal Shrestha <bikal3.bs@gmail.com>
2019-07-20 11:06:41 +0545

    profile image  added

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -44,32 +44,6 @@ $("#appointment").submit(function(event) {
 
 
 
-//api for editing profile
-$("#profileDetail").submit(function(event) {
-    event.preventDefault();
-    var _form = $(this);
-    var data = _form.serialize();
-    data += '&_id=' + localStorage.getItem("_id") + '&token=' + localStorage.getItem("token");
-    console.log(data);
-    $.ajax({
-        type: 'POST',
-        url: 'http://127.0.0.1:3000/api/edtProfile',
-        data: data,
-        dataType: "json",
-        beforeSend: function() {
-            $(".main-btn").attr("disabled", true);
-        },
-        success: function(response) {
-            console.log(response);
-            $("#check").html(response.Success).addClass("alert");
-            if (response.Success == "Profile Updated Successfully!!") {
-                localStorage.setItem("username", response.username);
-            }
-            $(".main-btn").attr("disabled", false);
-        },
-        error: function() {}
-    })
-});
 
 
 //api for posting on forum
